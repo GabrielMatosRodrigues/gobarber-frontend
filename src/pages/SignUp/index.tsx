1643f27655b17f16ff6e115110eb9e88c5bc21d1
Gabriel Matos Rodrigues <gabrielm1212@gmail.com>
2020-06-18 14:22:15 -0300

    Login e cadastro finalizado

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -3,19 +3,34 @@ import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
+import { Link, useHistory } from 'react-router-dom';
+
+
+import api from '../../services/api';
+
+import { useToast } from '../../hooks/toast';
+
 import getValidationErrors from '../../utils/getValidationErrors';
 
-import { Container, Content, Background } from './styles';
+import { Container, Content, AnimationContainer, Background } from './styles';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.svg';
 
+interface SignUpFormData {
+    name: string;
+    email: string;
+    password: string;
+}
+
 const SignUp: React.FC = () => {
     const formRef = useRef<FormHandles>(null);
+    const { addToast } = useToast()
+    const history = useHistory();
 
-    const handleSubmit = useCallback(async (data: object) => {
+    const handleSubmit = useCallback(async (data: SignUpFormData) => {
         try {
             formRef.current?.setErrors({});
 
@@ -29,14 +44,32 @@ const SignUp: React.FC = () => {
                 abortEarly: false,
             });
 
+            await api.post('/users', data);
+            
+            history.push('/');
+
+            addToast({
+                type: 'success',
+                title: 'Caadastro realizado!',
+                description: 'Você já pode fazer seu logon no GoBarber!'
+            })
+            
         }catch(err) {
-            console.log(err);
+            if(err instanceof Yup.ValidationError) {
+                const errors = getValidationErrors(err);
 
-            const errors = getValidationErrors(err);
+                formRef.current?.setErrors(errors);
 
-            formRef.current?.setErrors(errors);
+                return;
+            }
+
+            addToast({
+                type: 'error',
+                title: 'Erro no cadastro',
+                description: 'Ocorreu um erro ao fazer cadastro, tente novamente.',
+            });
         }
-    }, []);
+    }, [addToast, history]);
 
     return (
         <Container>
@@ -44,22 +77,24 @@ const SignUp: React.FC = () => {
             <Background />
 
             <Content>
-                <img src={logoImg} alt="GoBarber" />
+                <AnimationContainer>
+                    <img src={logoImg} alt="GoBarber" />
 
-                <Form ref={formRef} onSubmit={handleSubmit}>
-                    <h1>Faça seu cadastro</h1>
+                    <Form ref={formRef} onSubmit={handleSubmit}>
+                        <h1>Faça seu cadastro</h1>
 
-                    <Input name="name" icon={FiUser} placeholder="Nome" />
-                    <Input name="email" icon={FiMail} placeholder="Email" />
-                    <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
+                        <Input name="name" icon={FiUser} placeholder="Nome" />
+                        <Input name="email" icon={FiMail} placeholder="Email" />
+                        <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
 
-                    <Button type="submit">Cadastrar</Button>
-                </Form>
+                        <Button type="submit">Cadastrar</Button>
+                    </Form>
 
-                <a href="login">
-                    <FiArrowLeft />
-                    Voltar para Logon
-                </a>
+                    <Link to="/">
+                        <FiArrowLeft />
+                        Voltar para Logon
+                    </Link>
+                </AnimationContainer>
             </Content>
         </Container>
     )    
---
+++ src/routes/Route.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Redirect, RouteProps as ReactDOMRouteProps, Route as ReactDOMRoute } from 'react-router-dom';
+
+import { useAuth } from '../hooks/auth';
+
+interface RouteProps extends ReactDOMRouteProps {
+    isPrivate?: boolean;
+    component: React.ComponentType;
+}
+
+const Route: React.FC<RouteProps> = ({ component: Component, isPrivate = false, ... rest }) => {
+    const { user } = useAuth();
+
+    return (
+        <ReactDOMRoute {...rest}
+        render={({ location }) => {
+            return isPrivate === !!user ? (
+                <Component />
+            ) : (
+                <Redirect to={{ pathname: isPrivate ? '/' : '/dashboard', state: { from: location } }} />
+            )
+        }} 
+        />
+    );
+};
+
+export default Route;
